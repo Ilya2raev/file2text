390a20685af311355957cb1c65b344aaedb33638
Ilya2raev <doshilya@gmail.com>
2023-04-11 16:54:36 +0700

    Second commit

---
+++ image_pdf.py
@@ -0,0 +1,22 @@
+import pdf2image
+import pytesseract
+import os
+
+from filename import filename
+from write_file import write_file
+
+def image_like_pdf_to_text(filename):
+    pytesseract.pytesseract.tesseract_cmd = os.path.join(os.getcwd(), r"tesseract\tesseract.exe")
+    image = pdf2image.convert_from_path(filename, 500, 
+                                poppler_path=os.getcwd()+r'\poppler-0.68.0\bin')
+    result = ''
+
+    try:
+        for page in image:
+            result += pytesseract.image_to_string(page, lang='eng+rus')
+
+    finally:
+        write_file(result)
+        return result
+        
+print(image_like_pdf_to_text(filename))
